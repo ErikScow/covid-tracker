6767a20a4110b3f2d6340d3e07b0f4c70d1502d0
ErikScow <erikscow@gmail.com>
2021-01-17 15:38:05 -0800

    Added more graph data types to graph feature

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,9 +10,8 @@ import axios from 'axios'
 
 import { Context } from './contexts/context'
 
-import {
-  convertStateName
-} from './utils/stringModifiers'
+import { convertStateName } from './utils/stringModifiers'
+import { statePopulationHash } from './utils/data'
 
 import Nav from './components/Nav'
 import ContentContainer from './components/ContentContainer'
@@ -27,8 +26,11 @@ function App() {
   const [graphDataStates, setGraphDataStates] = useState([])
   const [graphDataCountries, setGraphDataCountries] = useState([])
   const [clipBoard, setClipBoard] = useState({
-    cases:[{locationName: '', x: new Date(), y:0}],
-    deaths: [{locationName: '', x: new Date(), y:0}]
+    'cases':[{locationName: '', x: new Date(), y:0}],
+    'deaths': [{locationName: '', x: new Date(), y:0}],
+    'casesPerMil': [{locationName: '', x: new Date(), y:0}],
+    'deathsPerMil': [{locationName: '', x: new Date(), y:0}],
+    'deathRate': [{locationName: '', x: new Date(), y:0}],
   })
 
   const [currentLocationType, setCurrentLocationType] = useState('countries')
@@ -74,67 +76,6 @@ function App() {
     axios.get('https://covid.erikscow.com/proxy/states')
       .then(res => {
         const data = res.data
-        const statePopulationHash = {
-          'AL': 4903185,
-          'AK': 731545,
-          'AS': 55312,
-          'AZ': 7278717,
-          'AR': 3017804,
-          'CA': 39512223,
-          'CO': 5758736,
-          'CT': 3565287,
-          'DE': 973764,
-          'DC': 705749,
-          'FM': 104929,
-          'FL': 21477737,
-          'GA': 10617423,
-          'GU': 167294,
-          'HI': 1415872,
-          'ID': 1787065,
-          'IL': 12671821,
-          'IN': 6732219,
-          'IA': 3155070,
-          'KS': 2913314,
-          'KY': 4467673,
-          'LA': 4648794,
-          'ME': 1344212,
-          'MH': 58791,
-          'MD': 6045680,
-          'MA': 6892503,
-          'MI': 9986857,
-          'MN': 5639632,
-          'MS': 2976149,
-          'MO': 6137428,
-          'MT': 1068778,
-          'NE': 1934408,
-          'NV': 3080156,
-          'NH': 1359711,
-          'NJ': 8882190,
-          'NM': 2096829,
-          'NY': 19453561,
-          'NC': 10488084,
-          'ND': 762062,
-          'MP': 51994,
-          'OH': 11689100,
-          'OK': 3956971,
-          'OR': 4217737,
-          'PW': 18008,
-          'PA': 12801989,
-          'PR': 3193694,
-          'RI': 1059361,
-          'SC': 5148714,
-          'SD': 884659,
-          'TN': 6829174,
-          'TX': 28995881,
-          'UT': 3205958,
-          'VT': 623989,
-          'VI': 104578,
-          'VA': 8535519,
-          'WA': 7614893,
-          'WV': 1792147,
-          'WI': 5822434,
-          'WY': 578759
-          }
         const formattedData = []
         for( let i=0; i<data.length; i++ ){
           const willNotBeSaved = (data[i].state === 'GU' || data[i].state === 'PR' || data[i].state === 'MP' || data[i].state === 'AS' || data[i].state === 'DC' || data[i].state === 'VI')
--- front-end/src/components/GraphContainer.js
+++ front-end/src/components/GraphContainer.js
@@ -4,6 +4,7 @@ import Select from 'react-select'
 import { Context } from '../contexts/context'
 
 import { convertStateName } from '../utils/stringModifiers'
+import { statePopulationHash } from '../utils/data'
 
 import GraphComponent from './GraphComponent'
 
@@ -19,12 +20,16 @@ const GraphContainer = (props) => {
     const [clipBoardStatus, setClipBoardStatus] = useState(false)
 
     const [currentGraphData, setCurrentGraphData] = useState({
-        cases:[{locationName: '', x: new Date(), y:0}],
-        deaths: [{locationName: '', x: new Date(), y:0}]
+        'cases':[{locationName: '', x: new Date(), y:0}],
+        'deaths': [{locationName: '', x: new Date(), y:0}],
+        'casesPerMil': [{locationName: '', x: new Date(), y:0}],
+        'deathsPerMil': [{locationName: '', x: new Date(), y:0}],
     })
 
     const [countryCasesData, setCountryCasesData] = useState([{locationName: '', x: new Date(), y:0}])
     const [countryDeathsData, setCountryDeathsData] = useState([{locationName: '', x: new Date(), y:0}])
+    const [countryCasesPerMilData, setCountryCasesPerMilData] = useState([{locationName: '', x: new Date(), y:0}])
+    const [countryDeathsPerMilData, setCountryDeathsPerMilData] = useState([{locationName: '', x: new Date(), y:0}])
 
     const [graphDataType, setGraphDataType] = useState('cases')
 
@@ -38,6 +43,7 @@ const GraphContainer = (props) => {
                     .then(res => {
                         const caseData = res.data.All.dates
                         const formattedDataCases = []
+                        const formattedDataCasesPerMil = []
                         for(const date in caseData){
                             if(parseInt(date.slice(8))%3===0){
                                 formattedDataCases.push({
@@ -45,19 +51,26 @@ const GraphContainer = (props) => {
                                     x: new Date(parseInt(date.slice(0,4)), parseInt(date.slice(5,7))-1, parseInt(date.slice(8))),
                                     y: caseData[date]
                                 })
+                                formattedDataCasesPerMil.push({
+                                    locationName: props.locationName,
+                                    x: new Date(parseInt(date.slice(0,4)), parseInt(date.slice(5,7))-1, parseInt(date.slice(8))),
+                                    y: (caseData[date]/res.data.All.population) * 1000000,
+                                })
                             }
                             
                         }
                         setCountryCasesData(formattedDataCases)
+                        setCountryCasesPerMilData(formattedDataCasesPerMil)
                         let countryData
                         if (props.abbreviation in graphCountriesStore){
                             let newGraphCountriesStore = graphCountriesStore
                             let item = newGraphCountriesStore[props.abbreviation]
-                            item.cases = formattedDataCases
+                            item['cases'] = formattedDataCases
+                            item['casesPerMil'] = formattedDataCasesPerMil
                             newGraphCountriesStore = {...newGraphCountriesStore, item}
                             setGraphCountriesStore(newGraphCountriesStore)
                         } else {
-                            countryData = {cases: formattedDataCases}
+                            countryData = {'cases': formattedDataCases, 'casesPerMil': formattedDataCasesPerMil}
                             let newGraphCountriesStore = graphCountriesStore
                             newGraphCountriesStore[props.abbreviation] = countryData
                             setGraphCountriesStore(newGraphCountriesStore)
@@ -71,6 +84,7 @@ const GraphContainer = (props) => {
                     .then(res => {
                         const deathsData = res.data.All.dates
                         const formattedDataDeaths = []
+                        const formattedDataDeathsPerMil = []
                         for(const date in deathsData){
                             if (parseInt(date.slice(8))%3===0){
                                 formattedDataDeaths.push({
@@ -78,19 +92,26 @@ const GraphContainer = (props) => {
                                     x: new Date(parseInt(date.slice(0,4)), parseInt(date.slice(5,7))-1, parseInt(date.slice(8))),
                                     y: deathsData[date]
                                 }) 
+                                formattedDataDeathsPerMil.push({
+                                    locationName: props.locationName,
+                                    x: new Date(parseInt(date.slice(0,4)), parseInt(date.slice(5,7))-1, parseInt(date.slice(8))),
+                                    y: (deathsData[date]/res.data.All.population) * 1000000,
+                                })
                             }
                             
                         }
                         setCountryDeathsData(formattedDataDeaths)
+                        setCountryDeathsPerMilData(formattedDataDeathsPerMil)
                         let countryData
                         if (props.abbreviation in graphCountriesStore){
                             let newGraphCountriesStore = graphCountriesStore
                             let item = newGraphCountriesStore[props.abbreviation]
-                            item.deaths = formattedDataDeaths
+                            item['deaths'] = formattedDataDeaths
+                            item['deathsPerMil'] = formattedDataDeathsPerMil
                             newGraphCountriesStore = {...newGraphCountriesStore, item}
                             setGraphCountriesStore(newGraphCountriesStore)
                         } else {
-                            countryData = {deaths: formattedDataDeaths}
+                            countryData = {'deaths': formattedDataDeaths, 'deathsPerMil': formattedDataDeathsPerMil}
                             let newGraphCountriesStore = graphCountriesStore
                             newGraphCountriesStore[props.abbreviation] = countryData
                             setGraphCountriesStore(newGraphCountriesStore)
@@ -110,8 +131,11 @@ const GraphContainer = (props) => {
                         const data = res.data
                         const formattedDataCases = []
                         const formattedDataDeaths = []
+                        const formattedDataCasesPerMil = []
+                        const formattedDataDeathsPerMil = []
                         for( let i=0; i<data.length;i++){
                             const dateString = data[i].date.toString()
+                            let statePopulation = statePopulationHash[`${data[i].state}`]
                             if (parseInt(dateString.slice(6))%3===0){
                                 formattedDataCases.push({
                                     locationName: convertStateName(data[i].state),
@@ -123,17 +147,31 @@ const GraphContainer = (props) => {
                                     x: new Date(parseInt(dateString.slice(0,4)), parseInt(dateString.slice(4,6))-1, parseInt(dateString.slice(6))),
                                     y: data[i].death
                                 })
+                                formattedDataCasesPerMil.push({
+                                    locationName: convertStateName(data[i].state),
+                                    x: new Date(parseInt(dateString.slice(0,4)), parseInt(dateString.slice(4,6))-1, parseInt(dateString.slice(6))),
+                                    y: (data[i].positive/statePopulation)*1000000
+                                })
+                                formattedDataDeathsPerMil.push({
+                                    locationName: convertStateName(data[i].state),
+                                    x: new Date(parseInt(dateString.slice(0,4)), parseInt(dateString.slice(4,6))-1, parseInt(dateString.slice(6))),
+                                    y: (data[i].death/statePopulation)*1000000
+                                })
                             }
                             
                         }
                         setCurrentGraphData({
-                            cases: formattedDataCases,
-                            deaths: formattedDataDeaths
+                            'cases': formattedDataCases,
+                            'deaths': formattedDataDeaths,
+                            'casesPerMil': formattedDataCasesPerMil,
+                            'deathsPerMil': formattedDataDeathsPerMil,
                         })
                         let newGraphStatesStore = graphStatesStore
                         newGraphStatesStore[props.abbreviation] = {
-                            cases: formattedDataCases,
-                            deaths: formattedDataDeaths
+                            'cases': formattedDataCases,
+                            'deaths': formattedDataDeaths,
+                            'casesPerMil': formattedDataCasesPerMil,
+                            'deathsPerMil': formattedDataDeathsPerMil,
                         }
                         setGraphStatesStore(newGraphStatesStore)
                         setIsLoading(false)
@@ -147,10 +185,12 @@ const GraphContainer = (props) => {
 
     useLayoutEffect(() => {
         setCurrentGraphData({
-            cases:countryCasesData,
-            deaths:countryDeathsData
+            'cases':countryCasesData,
+            'deaths':countryDeathsData,
+            'casesPerMil':countryCasesPerMilData,
+            'deathsPerMil':countryDeathsPerMilData
         })
-    }, [countryCasesData, countryDeathsData])
+    }, [countryCasesData, countryDeathsData, countryCasesPerMilData, countryDeathsPerMilData])
 
     const toggleClipBoard = () => {
         if(clipBoardStatus){
@@ -173,15 +213,16 @@ const GraphContainer = (props) => {
     //options array for select component
     const dataTypeOptions = [
         { value: 'cases', label: 'Cases' },
-        { value: 'deaths', label: 'Deaths' }
+        { value: 'deaths', label: 'Deaths' },
+        { value: 'casesPerMil', label: 'Cases/Mil' },
+        { value: 'deathsPerMil', label: 'Deaths/Mil' }
     ]
 
     if(isLoading){
         return(
             <div className='loader'></div>
         )
-    } else if(graphDataType === 'cases'){
-        if(!clipBoardStatus){
+    } else if(!clipBoardStatus){
             return(
                 <div className='graph-outer-container'>
                     <div className='top-row'>
@@ -230,7 +271,7 @@ const GraphContainer = (props) => {
                             <button onClick={toggleClipBoard}>{clipBoardButton}</button>
                         </div>
                     </div>
-                    <GraphComponent data={{currentSet: currentGraphData.cases}}/>
+                    <GraphComponent data={{currentSet: currentGraphData[graphDataType]}}/>
                 </div>
             )
         }
@@ -282,116 +323,10 @@ const GraphContainer = (props) => {
                         <button onClick={toggleClipBoard}>{clipBoardButton}</button>
                     </div>
                 </div>
-                <GraphComponent data={{currentSet: currentGraphData.cases, comparisonSet: clipBoardData.cases}}/>
+                <GraphComponent data={{currentSet: currentGraphData[graphDataType], comparisonSet: clipBoardData[graphDataType]}}/>
             </div>
         )
-    } else if(graphDataType === 'deaths'){
-        if(!clipBoardStatus){
-            return(
-                <div className='graph-outer-container'>
-                    <div className='top-row'>
-                    <Select 
-                        className='select'
-                        options={dataTypeOptions}
-                        onChange={changeDataType}
-                        defaultValue={{ value: 'cases', label:'Cases' }}
-                        isSearchable={false}
-                        theme={(theme) => ({
-                            ...theme,
-                            colors: {
-                            ...theme.colors,
-                              primary25: '#daedee',
-                              primary: '#40a0a0',
-                              primary50: '#daedee'
-                            },
-                          })}
-                        styles={{
-                            control: (provided, state) => ({
-                                ...provided,
-                                minHeight: '30px',
-                                height: '30px',
-                                boxShadow: state.isFocused ? null : null,
-                              }),
-                          
-                              valueContainer: (provided, state) => ({
-                                ...provided,
-                                height: '30px',
-                                padding: '0 6px'
-                              }),
-                          
-                              input: (provided, state) => ({
-                                ...provided,
-                                margin: '0px',
-                              }),
-                              indicatorsContainer: (provided, state) => ({
-                                ...provided,
-                                height: '30px',
-                              }),
-                          
-                        }}
-                    />
-                        <div className='button-container'>
-                            <button onClick={saveToClipBoard}>Save</button>
-                            <button onClick={toggleClipBoard}>{clipBoardButton}</button>
-                        </div>
-                    </div>
-                    
-                    <GraphComponent data={{currentSet: currentGraphData.deaths}}/>
-                </div>
-            )
-        }
-
-        return(
-            <div className='graph-outer-container'>
-                <div className='top-row'>
-                    <Select 
-                        className='select'
-                        options={dataTypeOptions}
-                        onChange={changeDataType}
-                        defaultValue={{ value: 'cases', label:'Cases' }}
-                        isSearchable={false}
-                        theme={(theme) => ({
-                            ...theme,
-                            colors: {
-                            ...theme.colors,
-                              primary25: '#daedee',
-                              primary: '#40a0a0',
-                            },
-                          })}
-                        styles={{
-                            control: (provided, state) => ({
-                                ...provided,
-                                minHeight: '30px',
-                                height: '30px',
-                                boxShadow: state.isFocused ? null : null,
-                              }),
-                          
-                              valueContainer: (provided, state) => ({
-                                ...provided,
-                                height: '30px',
-                                padding: '0 6px'
-                              }),
-                          
-                              input: (provided, state) => ({
-                                ...provided,
-                                margin: '0px',
-                              }),
-                              indicatorsContainer: (provided, state) => ({
-                                ...provided,
-                                height: '30px',
-                              }),
-                          
-                        }}
-                    />
-                    <div className='button-container'>
-                        <button onClick={toggleClipBoard}>{clipBoardButton}</button>
-                    </div>
-                </div>
-                
-                <GraphComponent data={{currentSet: currentGraphData.deaths, comparisonSet: clipBoardData.deaths}}/>
-            </div>
-        )
-    }
+    
 
     
 }
